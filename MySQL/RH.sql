11d82f810c90f6ae626419ce26a0941fad5f698f
unknown <lipefelps2002@gmail.com>
2021-08-23 20:57:44 -0300

    lista1exercicio1

--- MySQL/RH.sql
+++ MySQL/RH.sql
@@ -1,3 +1,23 @@
-use db_rh;
+use recursosHumanos;
 
-select * from tb_funcionarios;
+create table tb_funcionarios (
+id bigint AUTO_INCREMENT,
+nome varchar(30) not NULL,
+habilidade varchar(30) not NULL,
+ salario decimal(10,4),
+ );
+ 
+INSERT INTO tb_funcionarios (nome,habilidade,salario) VALUES ("Leticia","Foco","4.000");
+INSERT INTO tb_funcionarios (nome,habilidade,salario) VALUES ("Fabio","Socialização","2.000");
+INSERT INTO tb_funcionarios (nome,habilidade,salario) VALUES ("Felipe","Apresentação","3.000");
+INSERT INTO tb_funcionarios (nome,habilidade,salario) VALUES ("Gih","Foco","3.500");
+INSERT INTO tb_funcionarios (nome,habilidade,salario) VALUES ("Lisboa","Detalhista","2.500");
+
+
+
+
+
+
+ 
+
+
